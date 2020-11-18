174a2fb794d6875633dd408680402084882c9ed0
Andreas Patsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2020-11-18 11:38:30 +0200

    updates

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # run in terminal: pip install numpy==1.19.3
 
@@ -10,8 +11,42 @@ if __name__ == '__main__':
         'oranges': [0, 3, 7, 2]
     }
 
-    purchases = pd.DataFrame(data)
+    purchases = pd.DataFrame(data, index=['June', 'Robert', 'Lily', 'David'])
 
     print(purchases)
 
+    print("\n-----------")
+    print(purchases.loc['June'])
 
+    print("\n-----------")
+    print(purchases.info())
+
+    print("\n-----------")
+    print(purchases.shape)
+
+    print("\n-----------")
+    print(purchases.columns)
+
+    print("\n-----------")
+    print(purchases.isnull())
+
+    # Removing null values
+    purchases.dropna()
+
+    print("\n-----------")
+    print(purchases.describe())
+    #count, mean, std, min, max
+
+    plt.rcParams.update({'font.size': 20, 'figure.figsize': (10, 8)})  # set font and plot size to be larger
+    purchases.plot(kind='scatter', x='apples', y='oranges', title='Revenue (millions) vs Rating')
+    # purchases['apples'].plot(kind='hist', title='Rating')
+
+    df = pd.read_csv('C:/Users/apatsimas/Desktop/projects/sam/imports/asd.csv')
+    print("\n-----------")
+    print(df)
+
+    df.to_json('new_purchases.json')
+
+    df_json = pd.read_json('new_purchases.json')
+    print("\n-----------")
+    print(df_json)
